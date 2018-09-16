7e2e6ff78e0fc6721a7218c6ba587d701c699a1c
root <amaurybsouza@gmail.com>
2018-09-16 17:49:22 -0300

    enviando script de relatório

---
+++ CursoShellScript/ScriptsAmaury/RelatorioMaquina.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#############################################################
+#                                                           #
+# NOME: RelatorioMaquina.sh                                 #
+#                                                           # 
+# AUTOR: Amaury Borges Souza (amaurybsouza@gmail.com)       #
+#                                                           #
+# DATA: 16/09/2018                                          #
+#                                                           #
+# DESCRIÇÃO: O script gera um relatório com as principais   #
+#            informações do servidor Linux/Debian.          #
+#                                                           #
+# USO: ./RelatorioMaquina.sh                                #
+#############################################################
+
+
+
+
