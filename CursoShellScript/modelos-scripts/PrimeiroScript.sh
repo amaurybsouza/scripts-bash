a7115a91de5a2cb9af4150ca9950b7baed709c94
root <amaurybsouza@gmail.com>
2018-09-16 13:06:49 -0300

    iniciando criação de scripts

---
+++ CursoShellScript/modelos-scripts/PrimeiroScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+echo "===== MEU PRIMEIRO SCRIPT ====="
+echo ""
+echo -n "Exibir data e hora atual: "
+date
+echo "==============================="
+echo "Listagem de Alunos: "
+sort ../arquivos/alunos2.txt
