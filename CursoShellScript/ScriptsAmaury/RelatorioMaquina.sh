405aed7a15bab5d6aae0cc86b0417c4829014fe4
root <amaurybsouza@gmail.com>
2018-09-16 21:35:53 -0300

    enviando script de relatório

--- CursoShellScript/ScriptsAmaury/RelatorioMaquina.sh
+++ CursoShellScript/ScriptsAmaury/RelatorioMaquina.sh
@@ -14,6 +14,31 @@
 # USO: ./RelatorioMaquina.sh                                #
 #############################################################
 
+KERNEL=$(uname -r)
+HOSTNAME=$(hostaname)
+CPUNO=$(cat /proc/cpuinfo | grep "model name" | wc -l)
+CPUMODEL=$(cat /proc/cpuinfo | grep "model name" | head -n1 | cut -c14-)
+MEMTOTAL=$(expr $(cat /proc/meminfo | grep MemTotal | tr -d ' ' | cut -d: -f 2 | tr -d kb)) # Em MB
+FILESYS=$(df -h | egrep -v '(tmpfs|udev)')
+UPTIME=$(uptime -s)
 
-
-
+clear
+echo "========================================================================="
+echo "Relatório da Máquina: $HOSTNAME"
+echo "Data/Hora: $(date)"
+echo "========================================================================="
+echo
+echo "Máquina ativa desde: $UPTIME"
+echo
+echo "Versão do kernel: $KERNEL"
+echo 
+echo "CPUs:"
+echo "Quantidade de CPUs/Core: $CPUNO"
+echo "Modelo da CPU: $CPUMODEL"
+echo 
+echo "Memória Total: $MEMTOTAL MB"
+echo
+echo "Partições:"
+echo "$FILESYS"
+echo
+echo "========================================================================="
