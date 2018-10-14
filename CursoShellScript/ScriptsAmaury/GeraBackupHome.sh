7b87369dfe55a6fb90ca6e51e5b59d12bb42f148
root <amaurybsouza@gmail.com>
2018-10-14 12:37:35 -0300

    enviando script para a cloud

--- CursoShellScript/ScriptsAmaury/GeraBackupHome.sh
+++ CursoShellScript/ScriptsAmaury/GeraBackupHome.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-#Exibição do Menu:
-clear
-echo "Escolha uma opção do menu:"
-echo
-echo "1 - Atualizar o sistema"
-echo "2 - Limpando pacotes do sistema"
-echo "3 - Escrevendo um teste com echo"
-echo "q - sair"
-echo
-read -p "Opção:" opcao
-echo
+#############################################################
+#                                                           #
+# NOME: GeraBackupHome.sh                                   #
+#                                                           # 
+# AUTOR: Amaury Borges Souza (amaurybsouza@gmail.com)       #
+#                                                           #
+# DATA: 02/10/2018                                          #
+#                                                           #
+# DESCRIÇÃO: O script gera um backup do home do usuário     #
+#                                                           #
+#                                                           #
+# USO: ./GeraBackupHome.sh                                  #
+#############################################################
 
-# Rotinas das Opções:
+DIRDEST=$HOME/Backup
 
-case "$opcao" in
-        1)
-                echo "Opção 1 escolhida"
-                echo "Atualizar o sistema"
-                sleep 3
-                ;;
-        2)
-                echo "Opção 2 escolhida"
-                echo "Limpando pacotes do sistema"
-                sleep 3
-                ;;
-        3)
-                echo "Opção 3 escolhida"
-                echo "Escrevendo um teste com echo"
-                sleep 3
-                ;;
-esac
+if [ ! -d $DIRDEST ]
+then
+	echo "Criando diretório $DIRDEST..."
+	mkdir -p $DIRDEST
+fi
 
+DAYS7=$(find $DIRDEST -ctime -7 -name backup_home\*tgz)
 
+if [ "$DAYS7" ] #testa a variavel se é nula.
+then
+	echo "Já foi gerado um backup do diretório $HOME nos últimos 7 dias."
+	echo -n "Deseja continuar? (N/s):"
+	read -n1 CONT
+	echo
+	if [ "$CONT"]
