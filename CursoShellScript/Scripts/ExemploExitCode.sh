5c6969d4daf40ce56ff2b7b703843746375d5146
root <amaurybsouza@gmail.com>
2018-10-22 21:16:06 -0300

    editando curso de shell

--- CursoShellScript/Scripts/ExemploExitCode.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-ls /tmp/
-
-ls /tmp/outroarquivo
-
-RETURN_CODE=$?
-
-echo "O codigo de retorno do ls foi $RETURN_CODE"
--- CursoShellScript/Scripts/ExemploFunction.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-maiuscula () {
-local VAR1=$(echo $1 | tr a-z A-Z)
-}
-
-maiuscula script
-echo $VAR1
