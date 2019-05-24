a36126b5220b4f3afdcff9f0531fafacc3080439
root <amaurybsouza@gmail.com>
2019-05-24 19:09:50 -0300

    adding new scripts

--- ScriptsTestes/for1.sh
+++ ScriptsTestes/for1.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
+#
+# for1.sh - Verifica os arquivos e diretórios do /etc
+#
+# Site:        https://medium.com/@amaurybsouza
+# Autor:       Amaury
+# Manutenção:  Amaury
+#
+# ------------------------------------------------------------------------ #
+#
+#
+#
+#
+#
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 03/10/2018, Mateus:
+#       - Início do programa
+#       - Conta com a funcionalidade X
+#   v1.1 10/10/2018, Lucas:
+#       - Alterado parametro XXXXX
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 4.4.19
+# ------------------------------------------------------------------------ #
+# Agradecimentos:
+#
+#       Joãozinho - Encontrou um bug na parte de login.
+#       Mariazinha - Enviou uma sugestão de adicionar o -h.
+# ------------------------------------------------------------------------ #
 
-
-clear 
-
+clear
 for valor in `ls /etc/*`
 do
 	echo O arquivo é: $valor
