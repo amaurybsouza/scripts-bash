34dd6939c8a2e78c2578986cdaff6682a3c0bef8
amaurybsouza <amaurybsouza@gmail.com>
2019-09-18 13:27:52 -0300

    new logs for network troubleshooting

--- scripts_novos/newtwork_troubleshooting.sh
+++ scripts_novos/newtwork_troubleshooting.sh
@@ -52,24 +52,42 @@ function menuprincipal () {
 
 		3)
 			function dns () {
-				read -p "Entre com uma URL: "
-				echo " "
-			       	if [ ] 
-					
-
+				ping -c5 google.com > /dev/null || ping -c5 8.8.8.8 > /dev/null
+				if [ $? -eq 0 ]
+				then
+					echo Resolvendo teste com nome!
+					sleep 3
+					echo Ping ok!
+				else
+					echo Ping not working
+				fi
 			}
+			dns
+			echo " "
+			read -n1 -r -p "Press any key to continue..."
+			menuprincipal
 			;;
 
-		3)
+		4)
 			function ping () {
 				sdsdsdsdsdsdsd
 			}
 			;;
 
-		4)
+		5)
 			echo Saindo do sistema...
 			sleep 2
 			exit 0
+			;;
+
+		6)
+			function rotas () {
+				ROUTE=`route -n | grep -A 1 -i roteador`
+				echo " "
+				echo -e "The default route:\n$ROUTE"
+			}
+			read -n1 -r -p "Press any key to continue..."
+			menuprincipal
 			;;
 
 	esac
